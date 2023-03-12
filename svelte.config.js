862e09a2fa76f3dea10f022a3575c36796be1c80
Arne Küderle <a.kuederle@gmail.com>
2023-03-12 13:01:40 +0100

    Fixed basepath

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -18,11 +20,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html',
-			paths: {
-				base: process.env.NODE_ENV === 'production' ? '/pwa-logger' : ''
-			}
-		})
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : '/pwa-logger'
+		}
 	}
 };
 
